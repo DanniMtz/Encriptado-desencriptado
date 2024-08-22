5b1763de6084098c78069d07697808cd8f3d69f0
DanniMtz <odamar_va@hotmail.com>
2024-08-22 05:39:32 -0600

    Extrae texto de areatexto

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+
+/* <!-- Uso de Selectores --> */
+
 var botonEncriptar = document.querySelector(".boton-encriptar");
 var botonDesencriptar = document.querySelector(".boton-desencriptar");
-var munieco = document.querySelector(".contenedormunieco");
+var munieco = document.querySelector(".contenedor-munieco");
 var contenedor = document.querySelector(".contenedor-parrafo");
 var resultado = document.querySelector(".texto-resultado");
 
+
+/* llamar las funciones al hacer click*/
+
+botonEncriptar.onclick = recuperarTexto;
+
+
+function recuperarTexto(){
+    var cajatexto = document.querySelector(".areatexto")
+    cajatexto.value
+   
+    alert (`Texto: ${cajatexto.value} `);
+}
