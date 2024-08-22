8da0d3a95baa7f7f8c78eaf9549c4ac5012e25ef
DanniMtz <odamar_va@hotmail.com>
2024-08-22 07:30:00 -0600

    Validacion de minusculas y funcion encriptar

--- script.js
+++ script.js
@@ -5,22 +5,72 @@ var botonEncriptar = document.querySelector(".boton-encriptar");
 var botonDesencriptar = document.querySelector(".boton-desencriptar");
 var munieco = document.querySelector(".contenedor-munieco");
 var contenedor = document.querySelector(".contenedor-parrafo");
+var botoncopiar = document.querySelector(".contenedor-copiar");
 var resultado = document.querySelector(".texto-resultado");
-
+var textArea = document.querySelector(".areatexto");
 
 /* llamar las funciones al hacer click*/
 
-botonEncriptar.onclick = resultadotext;
+botonEncriptar.onclick = encriptar;
 
 
-function resultadotext(){
-   var cajatexto = recuperarTexto()
-    resultado.textContent = cajatexto;
-    alert (`${ resultado.textContent}`)
+function encriptar(){
+    const caracteresOK = /^[a-z \n]+$/;
+    const esValido = caracteresOK.test(textArea.value); 
+
+    if (esValido){
+        const textoEncriptado = encriptarTexto(textArea.value)
+        resultado.textContent  = textoEncriptado;
+        textArea = "";
+        // resultado.style.backgroundImage = "none";
+        botonDesencriptar.disabled = false; 
+        botoncopiar.disabled = false; 
+}
+else{
+    mensajeDeError('Solo letras, sin acentos ni caracteres especiales y desactiva mayúsculas.');
+    textArea.value = "";
+    resultado.value = "";
+    botonDesencriptar.disabled = true; 
+    botoncopiar.disabled = true; 
+}
 }
 
+//Función para enviar mensaje de error en caso de no cumplir con requisito de ingreso de texto.
+function mensajeDeError(mensaje) {
+    const error = document.querySelector('#error');
+    error.textContent = mensaje;
+    error.style.display = 'block';
+    setTimeout(() => {
+        error.style.display = 'none';
+    }, 3000);
+}
 
-function recuperarTexto(){
-    var cajatexto = document.querySelector(".areatexto")
-    return cajatexto.value;
+function encriptarTexto(texto){
+    var texto = texto.toLowerCase()
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "ai"
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "enter"
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "imes"
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "ober"
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "ufat"
+        }
+        else{
+            textoFinal = textoFinal + texto[i]
+        }
+    }
+    alert(`Texto encriptado: ${textoFinal}`);
+    return textoFinal;
+   
+
 }
